d31fb87c189012732d7242f66e267729136ee823
vriesdejelmer <vriesdejelmer@gmail.com>
2024-10-11 20:02:03 +0200

    Thought I already commited. Implemented a decorator to track the open apps.  May have previously added the wrong file?

--- appLauncher.py
+++ appLauncher.py
@@ -36,14 +36,37 @@ from dataHandling.HistoryManagement.SpecBufferedManager import SpecbufferedManag
 from dataHandling.HistoryManagement.FinazonBufferedManager import FinazonBufferedDataManager 
 
 from apps.positionManaging.positionManager import PositionApp
-from IBConnector import IBConnector
+from ConnectionThreadManager import ConnectionThreadManager
 from TelegramBot import TelegramBot
 
 QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 QtWidgets.QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
+def appRunning(app_type, running_apps):
+    current_app = None
+    for app in running_apps:
+        if isinstance(app, app_type):
+            return app
 
-class AppLauncher(AppLauncherWindow, IBConnector):
+def app_opener(app_type):  # Decorator factory to accept 'app_type'
+    def decorator(func):
+        def wrapper(self):
+            current_app = appRunning(app_type, self.running_apps)
+            if not current_app:
+                new_app = func(self)
+                new_app.closing.connect(lambda: self.running_apps.remove(new_app))
+                self.running_apps.append(new_app)
+                new_app.show()
+            else:
+                current_app.activateWindow()
+                current_app.raise_()
+
+        return wrapper
+    return decorator
+
+
+
+class AppLauncher(AppLauncherWindow, ConnectionThreadManager):
 
     running_apps = []
     data_source = Constants.IB_SOURCE
@@ -93,54 +116,35 @@ class AppLauncher(AppLauncherWindow, IBConnector):
             self.toggleAppButtons(True, interface=Constants.FINAZON_SOURCE)
 
             self.data_source = Constants.FINAZON_SOURCE
-            
-
-    def appRunning(self,app_type):
-        current_app = None
-        for app in self.running_apps:
-            if isinstance(app, app_type):
-                current_app = app
-                break
-
-        if current_app is not None:
-            current_app.activateWindow()
-            current_app.raise_()
-            return True
-        else:
-            return False
 
 
+    @app_opener(OptionPositions)
     def openOptionPosApp(self):
         position_manager = self.getNewPositionManager()
-        new_app = OptionPositions(position_manager)
-        self.running_apps.append(new_app)
-        new_app.show()
+        return OptionPositions(position_manager)
     
 
+    @app_opener(OptionVisualization)
     def openOptionVizApp(self):
-        if not self.appRunning(OptionVisualization):
-            option_manager = self.getOptionManager()
-            symbol_manager = self.getNewSymbolManager(identifier='option_symbol_manager')
-            new_app = OptionVisualization(option_manager, symbol_manager)
-            self.running_apps.append(new_app)
-            new_app.show()
+        option_manager = self.getOptionManager()
+        symbol_manager = self.getNewSymbolManager(identifier='option_symbol_manager')
+        return OptionVisualization(option_manager, symbol_manager)
+            
 
+    # @app_opener(PortfolioManager)
+    # def openStocksApp(self):
+    #     position_manager = self.getNewPositionManager()
+    #     return PortfolioManager(position_manager)
+        
 
-    def openStocksApp(self):
-        position_manager = self.getNewPositionManager()
-        new_app = PortfolioManager(position_manager)
-        self.running_apps.append(new_app)
-        new_app.show()
-    
-    
+    @app_opener(PositionApp)
     def openPositionsApp(self):
         position_manager = self.getNewPositionManager()
         order_manager = self.getOrderManager()
-        new_app = PositionApp(position_manager, order_manager)
-        self.running_apps.append(new_app)
-        new_app.show()
+        return PositionApp(position_manager, order_manager)
+        
     
-
+    @app_opener(AlertManager)
     def openAlertApp(self):
         history_manager = self.getHistoryManager('general_history')
 
@@ -154,48 +158,35 @@ class AppLauncher(AppLauncherWindow, IBConnector):
         alert_app = AlertManager(buffered_manager, indicator_processor, QThread())
         if self.telegram_bot is not None:
             alert_app.setTelegramListener(self.telegram_signal)
-        self.running_apps.append(alert_app)
-        alert_app.show()
+        return alert_app
 
 
-
+    @app_opener(DataDownloader)
     def openDataDetailsApp(self):
-        new_app = DataDownloader(QThread())
-        self.running_apps.append(new_app)
-        new_app.show()
+        return DataDownloader(QThread())
+        
 
-
+    @app_opener(TradeMaker)
     def openManualTraderApp(self):
         order_manager = self.getOrderManager()
         history_manager = self.getHistoryManagerIB(identifier='manual_trader_history')
         symbol_manager = self.getNewSymbolManager(identifier='trader_symbol_manager')
-        new_app = TradeMaker(order_manager, history_manager, symbol_manager)
-        self.running_apps.append(new_app)
-        new_app.show()
+        return TradeMaker(order_manager, history_manager, symbol_manager)
 
 
+    @app_opener(MoversList)
     def openMoversApp(self):
-        if not self.appRunning(MoversList):
-            history_manager = self.getHistoryManager()
+        history_manager = self.getHistoryManager()
 
-            if self.data_source == Constants.IB_SOURCE:
-                buffered_manager = BufferedDataManager(history_manager)
-            elif self.data_source == Constants.FINAZON_SOURCE:
-                buffered_manager = FinazonBufferedDataManager(history_manager)
+        if self.data_source == Constants.IB_SOURCE:
+            buffered_manager = BufferedDataManager(history_manager)
+        elif self.data_source == Constants.FINAZON_SOURCE:
+            buffered_manager = FinazonBufferedDataManager(history_manager)
 
-            new_app = MoversList(buffered_manager, QThread())
-            self.running_apps.append(new_app)
-            new_app.show()
+        return MoversList(buffered_manager, QThread())
 
-
-    def cleanupClosedApp(self):
-        print("AppLauncher.cleanupClosedApp")
-        for app in self.running_apps:
-            if isinstance(app, MoversList):
-                self.running_apps.remove(app)
-                break
-
-
+    
+    @app_opener(ComparisonList)
     def openComparisonApp(self):
         
         history_manager = self.getHistoryManager()
@@ -204,26 +195,20 @@ class AppLauncher(AppLauncherWindow, IBConnector):
         elif self.data_source == Constants.FINAZON_SOURCE:
             buffered_manager = SpecbufferedManagerFZ(history_manager)
 
-        new_app = ComparisonList(buffered_manager, QThread())
+        comp_app = ComparisonList(buffered_manager, QThread())
         if self.telegram_bot is not None:
-            new_app.telegram_signal = self.telegram_signal
-            self.telegram_bot.incoming_message_signal.connect(new_app.processTelegram)
-        self.running_apps.append(new_app)
-        new_app.show()
+            comp_app.telegram_signal = self.telegram_signal
+            self.telegram_bot.incoming_message_signal.connect(comp_app.processTelegram)
+        return comp_app
 
 
+    @app_opener(ListManager)
     def openListManager(self):
         symbol_manager = self.getNewSymbolManager(identifier='list_symbol_manager')
         history_manager = self.getHistoryManagerIB()
         buffered_manager = BufferedDataManager(history_manager)
         option_manager = self.getOptionManager()
-        new_app = ListManager(symbol_manager, buffered_manager, option_manager)
-        self.running_apps.append(new_app)
-        new_app.show()
-
-
-    def historyUpdates(self, signal):
-        print(f"AppLauncher.historyUpdates: {signal}")
+        return ListManager(symbol_manager, buffered_manager, option_manager)
 
 
     def downloadShortRateData(self):
@@ -264,6 +249,7 @@ class AppLauncher(AppLauncherWindow, IBConnector):
     def closeEvent(self, *args, **kwargs):
         if self.telegram_bot is not None:
             self.telegram_bot.cleanupMessages()
+
 
 
 if __name__ == "__main__":
